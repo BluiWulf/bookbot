4214cc0612567a86498e333e8d494f09a7b69f8b
BluiWulf <bluiwulf@gmail.com>
2025-02-15 00:08:35 -0500

    adding count_char

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    print(word_count)
+    char_counts = count_char(text.lower())
+    print(char_counts)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +12,14 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_char(text):
+    char_counts = {}
+    for char in text:
+        if not char in char_counts:
+            char_counts[char] = 1
+        else:
+            char_counts[char] = char_counts[char] + 1
+    return char_counts
 
 main()
