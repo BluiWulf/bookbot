0c52150474256855244947b9875da7deb570b5a6
BluiWulf <bluiwulf@gmail.com>
2025-02-22 01:59:31 -0500

    adding updates to project that I missed previously

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-from stats import count_words
+import sys
+
+from stats import *
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     word_count = count_words(text)
     char_counts = count_char(text.lower())
-    print_report(book_path, word_count, char_counts)
+    sorted_counts = sort_entries(char_counts)
+    print_report(book_path, word_count, sorted_counts)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_char(text):
-    char_counts = {}
-    for char in text:
-        if not char in char_counts:
-            char_counts[char] = 1
-        else:
-            char_counts[char] = char_counts[char] + 1
-    return char_counts
-
-def print_report(book_path, word_count, char_counts):
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count} words found in the document\n")
-    for char in char_counts:
-        if char.isalpha():
-            print(f"The '{char}' character was found {char_counts[char]} times")
-    print("\n--- End report ---")
+def print_report(book_path, word_count, sorted_counts):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for counter in sorted_counts:
+        if counter["char"].isalpha():
+            print(f"{counter["char"]}: {counter["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_char(text):
+    char_counts = {}
+    for char in text:
+        if not char in char_counts:
+            char_counts[char] = 1
+        else:
+            char_counts[char] = char_counts[char] + 1
+    return char_counts
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_entries(char_counts):
+    sorted_list = []
+    for char in char_counts:
+        sorted_list.append({"char": char, "count": char_counts[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
